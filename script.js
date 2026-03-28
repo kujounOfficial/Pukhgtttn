a63e0f9a1b73caa5b7c2fe988ba1c319a40d03e9
kujounOfficial <168577284+kujounOfficial@users.noreply.github.com>
2026-03-28 08:37:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -390,7 +390,7 @@ function dodge() {
                 if (!state.dodge) return;
                 const inputPtr = args[1];
                 const now = Date.now();
-                const test = inputPtr.add(32).readS32();
+                const test = inputPtr.add(8).readS32();
                 showFloater(test.toString());
 
                 if(inputId != 2) return;
